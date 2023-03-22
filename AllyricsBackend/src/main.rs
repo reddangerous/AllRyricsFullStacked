74429e57973c5472f80aaf7c08c9e3a2e169e325
reddangerous <dkinyanjui144@gmail.com>
2023-03-22 18:54:18 +0300

    Updated Lyrics Fetching

--- AllyricsBackend/src/main.rs
+++ AllyricsBackend/src/main.rs
@@ -26,6 +26,7 @@ async fn main() -> std::io::Result<()> {
         let cors = Cors::default()
             .allowed_origin("http://localhost:3000")
             .allowed_origin("http://localhost:8080")
+            .allowed_origin("http://127.0.0.1:8080")
             .allowed_methods(vec!["GET", "POST"])
             .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
             .allowed_header(http::header::CONTENT_TYPE)
